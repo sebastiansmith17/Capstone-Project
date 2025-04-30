70687434400a7158e9edcbbaa050dd7136cf322d
sebastiansmith17 <89859084+sebastiansmith17@users.noreply.github.com>
2025-04-30 11:19:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:5500',
   'https://sebastiansmith17.github.io/Capstone-Project/',
+  'https://sebastiansmith17.github.io',
   'https://sebastiansmith17.github.io/Capstone-Project/client-info.html'
 ];
 
